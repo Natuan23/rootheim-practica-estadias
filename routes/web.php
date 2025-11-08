ccaba66dec9ff7de853f4850f1502c6e6db1fd6d
IAN NATHAN ROSAS CASAS <rosascasasiannathan203@gmail.com>
2025-11-08 15:55:55 -0600

    Feature: Estructura de Tareas (Modelo y Migracion)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Models\Tarea; //Importa el modelo
 
 Route::get('/', function () {
-    return view('welcome');
+
+    //Ejemplo de obtención de todas las tareas
+    $tareas = Tarea::all();
+
+    //Ejemplo de creación de una tarea (solo la primera vez que ejecutes)
+    Tarea::create(['titulo' => 'Preparar entrevista Root Heim', 'descripcion' => 'Repasar Git, Laravel y DB','completada' => false]);
+    return view('welcome', ['tareas' => $tareas]);
 });
